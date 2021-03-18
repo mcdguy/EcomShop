237f03c104dc39bfdf906aa49d0ed4d9beb67cbd
unknown <hritiksharma1718@gmail.com>
2021-03-18 11:43:41 +0530

    change

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.use((req,res)=>{
 })
 
 console.log(process.env.NODE_ENV);
-if(process.env.NODE_ENV || true){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
     app.get('*',function(req,res){
-        res.sendFile(path.join(__dirname,'client/build','index.html'));
+        res.sendFile(path.join(__dirname,'client','build'));
     })
 }
