97d99a082b2fdb38d3c21329e7e7508d94af1db4
unknown <hritiksharma1718@gmail.com>
2021-03-18 11:55:13 +0530

    msg

--- server.js
+++ server.js
@@ -26,11 +26,13 @@ app.use(express.urlencoded({extended: true}));
 app.use('/images',express.static('images'));
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+    
+}
+
+app.use(express.static('client/build'));
     app.get('*',function(req,res){
         res.sendFile(path.join(__dirname,'client/build','index.html'));
     })
-}
 
 app.use('/product',require('./routes/product-route'));
 app.use('/blog',require('./routes/blog-route'));
