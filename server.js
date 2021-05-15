3330624429d67d05d5ba76a9e25b467caec6cde3
unknown <hritiksharma1718@gmail.com>
2021-05-15 20:00:37 +0530

    message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-// const cors = require('cors');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
