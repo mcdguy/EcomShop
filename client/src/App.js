6940c1640574c2018e7f48337f8f8c46db2b5db9
unknown <hritiksharma1718@gmail.com>
2021-05-30 20:26:57 +0530

    message

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React,{Suspense , lazy} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './pages/Navbar';
 import Home from './pages/Home';
-import Shop from './pages/Shop';
 import Loader from './components/loader';
 import Login from './components/login';
 import Error from './pages/Error';
@@ -15,6 +14,8 @@ const Checkout = lazy(()=> import('./pages/Checkout'));
 const SingleProduct = lazy(()=> import('./pages/SingleProduct'));
 const Account = lazy(()=> import('./pages/Account'));
 const Forgot = lazy(()=> import('./pages/Forgot'));
+const Shop = lazy(()=> import('./pages/Shop'));
+// import Shop from './pages/Shop';
 // import SingleProduct from './pages/SingleProduct';
 // import Account from './pages/Account';
 // import Forgot from './pages/Forgot';
@@ -35,42 +36,41 @@ const App = () => {
                 <Home/>
               </Route>
 
-              <Route path="/shop" exact>
-                <Shop/>
-              </Route>
 
             <Suspense fallback={<Loader/>}>
-              <Route path="/about" exact>
-                <About/>
-              </Route>
+                
+                <Route path="/shop" exact>
+                  <Shop/>
+                </Route>
 
-              <Route path="/video-gallery" exact>
-                <Gallery/>
-              </Route>
-              
-              <Route path="/cart" exact>
-                <Cart/>
-              </Route>
+                <Route path="/about" exact>
+                  <About/>
+                </Route>
 
-              <Route path="/checkout" exact>
-                <Checkout/>
-              </Route>
+                <Route path="/video-gallery" exact>
+                  <Gallery/>
+                </Route>
+                
+                <Route path="/cart" exact>
+                  <Cart/>
+                </Route>
 
-              <Route path="/findastore" exact>
-                <Location/>
-              </Route>
+                <Route path="/checkout" exact>
+                  <Checkout/>
+                </Route>
 
-              <Route path="/shop/:id" children={<SingleProduct/>} exact></Route>
-              
-              <Route path="/reset-password/:id/:token" children={<Forgot/>} exact></Route>
-              
-              <Route path="/account" exact>
-                <Account/>
-              </Route>
-              
+                <Route path="/findastore" exact>
+                  <Location/>
+                </Route>
+
+                <Route path="/shop/:id" children={<SingleProduct/>} exact></Route>
+                
+                <Route path="/reset-password/:id/:token" children={<Forgot/>} exact></Route>
+                
+                <Route path="/account" exact>
+                  <Account/>
+                </Route>
             </Suspense>
-
-
 
               <Route path="*">
                 <Error/>
